3c5c1c68d4a6f98df2e3d9b4e2478c17a3f90f0c
Benjamin Alan Weaver <baweaver@lbl.gov>
2019-04-02 15:50:11 -0700

    adding skeleton to script

--- py/desispec/scripts/dts.py
+++ py/desispec/scripts/dts.py
@@ -1,13 +1,31 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # -*- coding: utf-8 -*-
 """
-desispec.scripts.delivery
-=========================
+desispec.scripts.dts
+====================
 
 Entry point for :command:`desi_dts`.
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
+import datetime as dt
 import os
+import subprocess as sub
+import sys
+import time
+from collections import namedtuple
+from desiutil.log import get_logger, DEBUG
+
+
+log = get_logger(timestamp=True)
+
+
+DTSDir = namedtuple('DTSDir', 'source, staging, destination, hpss')
+
+
+config = [DTSDir('/data/dts/exposures/raw',
+                 os.path.realpath(os.path.join(os.environ['DESI_ROOT'], 'spectro', 'staging', 'raw')),
+                 os.path.realpath(os.environ['DESI_SPECTRO_DATA']),
+                 'desi/spectro/data'),]
 
 
 def pack_args(options):
@@ -45,8 +63,8 @@ def pack_args(options):
     return opts
 
 
-def parse_delivery(*args):
-    """Parse command-line options for DTS delivery script.
+def _options(*args):
+    """Parse command-line options for DTS script.
 
     Parameters
     ----------
@@ -58,27 +76,32 @@ def parse_delivery(*args):
     :class:`argparse.Namespace`
         The parsed command-line options.
     """
-    from sys import argv
     from argparse import ArgumentParser
-    desc = "Script called by DTS when files are delivered."
-    prsr = ArgumentParser(prog=os.path.basename(argv[0]), description=desc)
-    prsr.add_argument('-n', '--nersc', default='edison', metavar='NERSC_HOST',
-                      help="Run a script on this NERSC system (default %(default)s).")
-    prsr.add_argument('-p', '--prefix', metavar='PREFIX', action='append',
-                      help="Prepend one or more commands to the night command.")
-    prsr.add_argument('-s', '--staging', metavar='DIR',
-                      default=os.path.join(os.environ['DESI_ROOT'],
-                                           'spectro', 'staging', 'raw'),
-                      help=("Staging directory containing night directories " +
-                            "(default %(default)s)."))
-    prsr.add_argument('filename', metavar='FILE',
-                      help='Filename with path of delivered file.')
-    prsr.add_argument('exposure', type=int, metavar='EXPID',
-                      help='Exposure number.')
-    prsr.add_argument('night', metavar='YYYYMMDD', help='Night ID.')
-    prsr.add_argument('nightStatus',
-                      choices=('start', 'update', 'end'),
-                      help='Start/end info.')
+    desc = "Transfer DESI raw data files."
+    prsr = ArgumentParser(prog=os.path.basename(sys.argv[0]), description=desc)
+    prsr.add_argument('-b', '--backup', metavar='H', type=int, default=20,
+                      help='UTC time in hours to trigger HPSS backups (default %(default)s:00 UTC).')
+    prsr.add_argument('-d', '--debug', action='store_true',
+                      help='Set log level to DEBUG.')
+    prsr.add_argument('-k', '--kill', metavar='FILE',
+                      default=os.path.join(os.environ['HOME'], 'stop_dts'),
+                      help="Exit the script when FILE is detected (default %(default)s).")
+    prsr.add_argument('-n', '--nersc', default='cori', metavar='NERSC_HOST',
+                      help="Trigger DESI pipeline on this NERSC system (default %(default)s).")
+    prsr.add_argument('-P', '--no-pipeline', action='store_false', dest='pipeline',
+                      help="Only transfer files, don't start the DESI pipeline.")
+    # prsr.add_argument('-p', '--prefix', metavar='PREFIX', action='append',
+    #                   help="Prepend one or more commands to the night command.")
+    prsr.add_argument('-s', '--sleep', metavar='M', type=int, default=10,
+                      help='Sleep M minutes before checking for new data (default %(default)s minutes).')
+    # prsr.add_argument('filename', metavar='FILE',
+    #                   help='Filename with path of delivered file.')
+    # prsr.add_argument('exposure', type=int, metavar='EXPID',
+    #                   help='Exposure number.')
+    # prsr.add_argument('night', metavar='YYYYMMDD', help='Night ID.')
+    # prsr.add_argument('nightStatus',
+    #                   choices=('start', 'update', 'end'),
+    #                   help='Start/end info.')
     if len(args) > 0:
         options = prsr.parse_args(args)
     else:  # pragma: no cover
@@ -122,45 +145,95 @@ def move_file(filename, dst):
     :class:`str`
         The value returned by :func:`shutil.move`.
     """
-    from os import mkdir
-    from os.path import exists, isdir
     from shutil import move
-    from desiutil.log import get_logger
-    log = get_logger()
-    if not exists(dst):
+    if not os.path.exists(dst):
         log.info("mkdir('{0}', 0o2770)".format(dst))
-        mkdir(dst, 0o2770)
+        os.mkdir(dst, 0o2770)
     log.info("move('{0}', '{1}')".format(filename, dst))
     return move(filename, dst)
 
 
 def main():
-    """Entry point for :command:`desi_dts_delivery`.
+    """Entry point for :command:`desi_dts`.
 
     Returns
     -------
     :class:`int`
         An integer suitable for passing to :func:`sys.exit`.
     """
-    from os import environ
-    from os.path import dirname, join
-    from subprocess import Popen
-    from desiutil.log import get_logger
-    log = get_logger()
-    options = parse_delivery()
-    remote_command = ['desi_{0.nightStatus}_night {0.night}'.format(options)]
-    if options.prefix is not None:
-        remote_command = options.prefix + remote_command
-    remote_command = ('(' +
-                      '; '.join([c + ' &> /dev/null' for c in remote_command]) +
-                      ' &)')
-    command = ['ssh', '-n', '-q', options.nersc_host, remote_command]
-    log.info("Received file {0.filename} with exposure number {0.exposure:d}.".format(options))
-    dst = join(environ['DESI_SPECTRO_DATA'], options.night)
-    log.info("Using {0} as raw data directory.".format(dst))
-    move_file(options.filename, dst)
-    exposure_arrived = check_exposure(dst, options.exposure)
-    if options.nightStatus in ('start', 'end') or exposure_arrived:
-        log.info("Calling: {0}.".format(' '.join(command)))
-        proc = Popen(command)
+    global log
+    options = _options()
+    if options.debug:
+        log.setLevel(DEBUG)
+    ssh = 'ssh'
+    # ssh = '/bin/ssh'
+    while True:
+        log.info('Starting transfer loop.')
+        if os.path.exists(options.kill):
+            log.info("%s detected, shutting down transfer daemon.", options.kill)
+            return 0
+        #
+        # Find symlinks at KPNO.
+        #
+        for d in config:
+            status_dir = os.path.join(os.path.dirname(d.staging), 'status')
+            cmd = [ssh, '-q', 'dts', '/bin/find', d.source, '-type', 'l']
+            log.debug(' '.join(cmd))
+            p = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
+            out, err = p.communicate()
+            links = sorted([x for x in out.decode('utf-8').split('\n') if x])
+            if links:
+                for l in links:
+                    exposure = os.path.basename(l)
+                    night = os.path.basename(os.path.dirname(l))
+                    #
+                    # New night detected?
+                    #
+                    n = os.path.join(d.staging, night)
+                    if not os.path.isdir(n):
+                        log.debug("os.makedirs('%s', exist_ok=True)", n)
+                        # os.makedirs(n, exist_ok=True)
+                    #
+                    # Has exposure already been transferred?
+                    #
+                    se = os.path.join(n, exposure)
+                    de = os.path.join(d.destination, night, exposure)
+                    if not os.path.isdir(se) and not os.path.isdir(de):
+                        cmd = ['/bin/rsync', '--verbose', '--no-motd',
+                               '--recursive', '--copy-dirlinks', '--times',
+                               '--omit-dir-times',
+                               'dts:'+os.path.join(d.source, night, exposure)+'/',
+                               se+'/']
+                        log.debug(' '.join(cmd))
+                        # p = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
+                        # out, err = p.communicate()
+                        # status = str(p.returncode)
+                        status = '0'
+                    else:
+                        log.info('%s already transferred.', se)
+                        status = 'done'
+                    if status == '0':
+                        pass
+                    elif status == 'done':
+                        #
+                        # Do nothing, successfully.
+                        #
+                        pass
+                    else:
+                        log.error('rsync problem detected!')
+            else:
+                log.warning('No links found, check connection.')
+            #
+            # Are any nights eligible for backup?
+            # 12:00 MST = 19:00 UTC.
+            # Plus one hour just to be safe, so after 20:00 UTC.
+            #
+            yesterday = (dt.datetime.now() - dt.timedelta(seconds=86400)).strftime('%Y%m%d')
+            now = int(dt.datetime.utcnow().strftime('%H'))
+            hpss_file = d.hpss.replace('/', '_')
+            ls_file = os.path.join(os.environ['CSCRATCH'], hpss_file + '.txt')
+            if now >= options.backup:
+                pass
+        # time.sleep(options.sleep*60)
+        time.sleep(options.sleep)
     return 0
